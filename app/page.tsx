31f853dfc07392972d16e18246791e2287288e7e
Echigrus <echigrus@gmail.com>
2025-11-27 00:35:31 +0300

    onto chapter 12

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import AcmeLogo from "@/app/ui/acme-logo";
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import styles from "@/app/ui/home.module.css";
-import { lusitana } from "./ui/fonts";
+import { lusitana } from "@/app/ui/fonts";
 import Image from "next/image";
 
 export default function Page() {
@@ -31,7 +31,6 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
           <Image
             src="/hero-desktop.png"
             width={1000}
